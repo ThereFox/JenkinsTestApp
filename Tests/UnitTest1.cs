b02570cf85a4a90e1fa250323f5bf8b3aec8f0ea
ThereFox <alexandrgrizunow@gmail.com>
2024-06-01 16:24:39 +0400

    add some tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -16,8 +16,43 @@ public class UnitTest1
         
         sut.UpdateQuality();
         
-
         Assert.True(ZeroQualityElementInput.All(ex => ex.Quality >= 0));
-
     }
+
+    [Fact]
+    public void LegendaryItemNotModificated()
+    {
+        //arrange
+        var initData = new List<Item>()
+        {
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+        };
+        var sut = new GildedRose(initData);
+        
+        //act
+        sut.UpdateQuality();
+        
+        //assert
+        Assert.True(initData.All(_ => _.Quality == 80));
+    }
+
+    [Fact]
+    public void WorkWithConjuredItems()
+    {
+        //arrange
+        var initItem = new List<Item>()
+        {
+            new Item() { Name = "Conjured", Quality = 10, SellIn = 10}
+        };
+        var sut = new GildedRose(initItem);
+        
+        //act
+        sut.UpdateQuality();
+        
+        //assert
+        Assert.True(initItem[0].Quality == 8);
+        
+    }
+    
 }
